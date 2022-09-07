231de706225173991b8f3a93357c8f1624a26a9e
mmhdnzne <mertcankose97@outlook.com>
2022-09-07 20:26:13 +0300

    fix files

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "../footer";
-import Header from "../header";
+import Footer from "../Footer";
+import Header from "../Header";
 import styles from "./style.module.css";
 
 const Layout = ({ children }) => {
